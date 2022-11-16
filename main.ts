a955d81867514abd24a872ae35967b12f9b7115a
bobthebuilder49 <117940970+bobthebuilder49@users.noreply.github.com>
2022-11-16 13:50:34 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,91 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showString("tree")
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showString("house ")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # # # # #
+        # . # . .
+        # . . . .
+        `)
+    basic.pause(100)
+    basic.showString("gun")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . # # # #
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showString("dog")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showString("box")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # # #
+        # . # . #
+        # # # # #
+        # . # . #
+        `)
+    basic.pause(100)
+    basic.showString("train")
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . # # # .
+        . # # # .
+        # # # # #
+        `)
+    basic.pause(100)
+    basic.showString("hat")
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showString("arrow")
+    basic.pause(100)
+    basic.showLeds(`
+        # . . # .
+        . . # # #
+        # . . # .
+        # . . # .
+        # . . # .
+        `)
+    basic.pause(100)
+    basic.showString("it")
+})
